23f4f169d76e3f4d2f8e9c17458d2a27f75e6d8d
Atanas Gemedzhiyski <nakata9559@hotmail.com>
2024-02-17 23:00:08 +0200

    Java Advanced Exam

---
+++ Advanced/Java Advanced/src/_21_MidExam/_02_ClearSkies.java
@@ -0,0 +1,92 @@
+package _21_MidExam;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _02_ClearSkies {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+        char[][] protectedAirspace = new char[size][size];
+        int[] currentPosition = new int[2];
+        int[] nextPosition = new int[2];
+        fillMatrix(protectedAirspace, currentPosition, scanner);
+        int numOfEnemies = 4;
+        int armor = 300;
+
+        while (armor  > 0 && numOfEnemies > 0) {
+            String direction = scanner.nextLine();
+            nextPosition = moveTo(nextPosition, currentPosition, direction);
+            char nextSymbol = protectedAirspace[nextPosition[0]][nextPosition[1]];
+            protectedAirspace[currentPosition[0]][currentPosition[1]] = '-';
+
+            switch (nextSymbol) {
+                case 'R':
+                    armor = 300;
+                    break;
+
+                case 'E':
+                    if (numOfEnemies > 1) {
+                        armor -= 100;
+                    }
+                    numOfEnemies--;
+                    break;
+            }
+
+            protectedAirspace[nextPosition[0]][nextPosition[1]] = 'J';
+            currentPosition = nextPosition;
+        }
+
+        if (numOfEnemies == 0) {
+            System.out.println("Mission accomplished, you neutralized the aerial threat!");
+        } else {
+            System.out.printf("Mission failed, your jetfighter was shot down! Last coordinates [%d, %d]!%n",
+                    currentPosition[0], currentPosition[1]);
+        }
+
+        printMatrix(protectedAirspace);
+    }
+
+    public static void fillMatrix(char[][] protectedAirspace, int[] currentPosition, Scanner scanner) {
+        for (int row = 0; row < protectedAirspace.length; row++) {
+            char[] currentRow = scanner.nextLine().toCharArray();
+            protectedAirspace[row] = currentRow;
+
+            for (int col = 0; col < protectedAirspace.length; col++) {
+                if (currentRow[col] == 'J') {
+                    currentPosition[0] = row;
+                    currentPosition[1] = col;
+                }
+            }
+        }
+    }
+
+    public static int[] moveTo(int[] nextPosition, int[] currentPosition, String direction) {
+        switch (direction) {
+            case "up":
+                nextPosition = new int[] {currentPosition[0] - 1, currentPosition[1]};
+                break;
+
+            case "down":
+                nextPosition = new int[] {currentPosition[0] + 1, currentPosition[1]};
+                break;
+
+            case "left":
+                nextPosition = new int[] {currentPosition[0], currentPosition[1] - 1};
+                break;
+
+            case "right":
+                nextPosition = new int[] {currentPosition[0], currentPosition[1] + 1};
+                break;
+        }
+        return nextPosition;
+    }
+
+    public static void printMatrix(char[][] protectedAirspace) {
+        Arrays.stream(protectedAirspace)
+                .forEach(row -> System.out.println(String.join("",
+                        Arrays.toString(row)
+                                .replaceAll("[\\[\\],\\s]", ""))));
+    }
+}
---
+++ Advanced/Java Advanced/src/_21_MidExam/_03_ChasingSharks/Basin.java
@@ -0,0 +1,62 @@
+package _21_MidExam._03_ChasingSharks;
+
+import java.util.*;
+
+public class Basin {
+    private String name;
+    private int capacity;
+    private List<Shark> sharks;
+
+    public Basin(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.sharks = new ArrayList<>();
+    }
+
+    public void addShark(Shark shark) {
+        if (this.capacity > this.sharks.size()) {
+            this.sharks.add(shark);
+        } else {
+            System.out.println("This basin is at full capacity!");
+        }
+    }
+
+    public boolean removeShark(String kind) {
+        return this.sharks.removeIf(shark -> shark.getKind().equals(kind));
+    }
+
+    public Shark getLargestShark() {
+        return this.sharks.stream()
+                .max(Comparator.comparing(Shark::getLength))
+                .orElse(null);
+    }
+
+    public Shark getShark(String kind) {
+        return this.sharks.stream()
+                .filter(shark -> shark.getKind().equals(kind))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int getCount() {
+        return this.sharks.size();
+    }
+
+    public int getAverageLength() {
+        return (int) this.sharks
+                .stream()
+                .mapToDouble(Shark::getLength)
+                .average()
+                .orElse(0.0);
+    }
+
+    public String report() {
+        return String.format("Sharks in %s:%n" +
+                "%s",
+                this.name,
+                String.join(System.lineSeparator(),
+                        this.sharks.stream()
+                                .map(String::valueOf)
+                                .toArray(String[]::new)));
+    }
+}
